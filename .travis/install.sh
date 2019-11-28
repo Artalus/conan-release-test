e5d9fe27beaa02c64067aa6c234b51af2a205e45
Artalus <artalus-mail@yandex.ru>
2019-11-28 09:49:59 +0300

    more fx

--- .travis/install.sh
+++ .travis/install.sh
@@ -5,21 +5,22 @@ set -x
 
 if [[ "$(uname -s)" == 'Darwin' ]]; then
     brew update || brew update
-    brew outdated pyenv || brew upgrade pyenv
-    brew install pyenv-virtualenv
+    # brew outdated pyenv || brew upgrade pyenv
+    # brew install pyenv-virtualenv
     brew install cmake || true
 
-    if which pyenv > /dev/null; then
-        eval "$(pyenv init -)"
-    fi
+    # if which pyenv > /dev/null; then
+    #     eval "$(pyenv init -)"
+    # fi
 
-    pyenv install 2.7.10 --skip-existing
-    pyenv virtualenv 2.7.10 conan --force
-    pyenv rehash
-    pyenv activate conan
+    # CFLAGS="-I$(brew --prefix openssl)/include" \
+    # LDFLAGS="-L$(brew --prefix openssl)/lib" \
+    # pyenv install 2.7.10 --skip-existing
+    # pyenv virtualenv 2.7.10 conan --force
+    # pyenv rehash
+    # pyenv activate conan
 fi
 
-pip install ninja --upgrade
 pip install conan --upgrade
 pip install conan-package-tools
 
